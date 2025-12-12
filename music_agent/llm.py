e251cbf650d76f0630461acf2669a23032fe816f
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-13 00:46:26 +0900

    gpt 모델 중 가장 의도에 맞게 동작하는 모델로 지정

--- music_agent/llm.py
+++ music_agent/llm.py
@@ -1,7 +1,6 @@
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
-    model="gpt-4o-2024-11-20",
-    temperature=0.0,
-    streaming=True
+    model="gpt-4o-2024-08-06",
+    temperature=0
 )
