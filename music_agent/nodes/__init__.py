50a15aec9c8d032c57c12cb7827a9e9a114998e3
heegu123 <kookminkhg@kookmin.ac.kr>
2026-01-12 17:49:23 +0900

    새로운 노드 패키지 import 경로에 추가

--- music_agent/nodes/__init__.py
+++ music_agent/nodes/__init__.py
@@ -1,13 +1,15 @@
-from music_agent.nodes.model_node import call_model
+from music_agent.nodes.model_node import context_agent_node
 from music_agent.nodes.tool_node import tool_node
-from music_agent.nodes.select_node import random_select_node
+from music_agent.nodes.select_node import selection_node
 from music_agent.nodes.reason_node import generate_reason_node
 from music_agent.nodes.filter_node import filter_remix_tracks_node
-from music_agent.nodes.preference_node import preference_analyzer_node
+from music_agent.nodes.preference_node import *
 
-__all__ = ["call_model",
+__all__ = ["context_agent_node",
            "tool_node",
-           "random_select_node",
+           "selection_node",
            "generate_reason_node",
            "filter_remix_tracks_node",
-           "preference_analyzer_node"]
+           "preference_analyzer_node",
+           "preference_search_node"
+           ]
